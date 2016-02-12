7eace82e3c8ac06b792220f290efea4c3af764ad
Antoine ROLLIN <ra408576@etu.unice.fr>
2016-02-12 10:14:36 +0100

    [Update] Maven - mvn package OK

---
+++ Statamutator/src/test/java/group01/MainTest.java
@@ -0,0 +1,13 @@
+package group01;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by user on 12/02/2016.
+ */
+public class MainTest extends TestCase {
+
+    public void testMain() throws Exception {
+
+    }
+}
