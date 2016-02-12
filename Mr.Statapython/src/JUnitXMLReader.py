a6677256eab1b6571ae4f803ad9f37e74e8f760d
Kevin Buisson <buisson.kevin2@gmail.com>
2016-02-12 11:59:28 +0100

    [Update] XML Parsing

--- Mr.Statapython/src/JUnitXMLReader.py
+++ Mr.Statapython/src/JUnitXMLReader.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python
 import os
-import xml.etree.ElementTree as XML
-
+import xml.etree.ElementTree as Xml
+import pprint
 
 class JUnitXMLReader:
     # def __init__(self):
 
     @staticmethod
     def read(xmlFile):
-        root = XML.parse(xmlFile).getroot()
+        # Parse XML file
+        root = Xml.parse(xmlFile).getroot()
 
-        print(root.attrib)
+        # Initialize test suite results
+        testsuite = root.attrib.copy()
+        testsuite['testcases'] = []
 
+        # Check every test case results
         for element in root.iter('testcase'):
-            print('\t',)
-            print(element.attrib)
-            print(len(element.findall('failure')))
+            # Initialize test case results
+            testcase = element.attrib.copy()
+
+            # Get errors
+            errors = element.findall('error')
+
+            # Failure
+            if len(element.findall('failure')) > 0:
+                testcase['status'] = 'failure'
+            # Error
+            elif len(errors) > 0:
+                testcase['status'] = 'error'
+                testcase['message'] = errors[0].attrib['message']
+            # Success
+            else:
+                testcase['status'] = 'success'
+
+            # Add the test case to the test suite
+            testsuite['testcases'].append(testcase)
+
+        # Return results
+        return testsuite
 
 
 if __name__ == "__main__":
     os.chdir('D:\\Google Drive\\Polytech\\_SI3\\_Semestre2\\OGL\\IslandExplorer\\target\\surefire-reports')
-    JUnitXMLReader.read(
-        'TEST-fr.unice.polytech.ogl.islcb.model.results.extras.TransformExtrasTest.xml'
+    pprint.pprint(
+        JUnitXMLReader.read(
+            'TEST-fr.unice.polytech.ogl.islcb.model.results.extras.TransformExtrasTest.xml'
+        )
     )
