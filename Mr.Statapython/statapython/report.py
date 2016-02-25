c3d37ac67aee00b7776f10aa021ee50e97e14c59
Kevin Buisson <buisson.kevin2@gmail.com>
2016-02-25 16:38:56 +0100

    [Update] Rendering report

--- Mr.Statapython/statapython/report.py
+++ Mr.Statapython/statapython/report.py
@@ -3,61 +3,51 @@ import argparse
 import os
 import sys
 
-from jinja2 import Environment, PackageLoader
-
+from .statarender import ReportRenderer
 from .stataxml import JUnitReport
 
-args = []  # Script arguments
 
-
-def main():
-    # Get report infos
-    report = JUnitReport.report(os.path.join(args.mutants_directory, args.testsdir))
-
-    # Display infos
-    print(report.testsuites)
-    print(report.counts)
-
-
-def render(base_report, mutations_reports):
-    # Get the report template
-    env = Environment(loader=PackageLoader('statapython', 'templates'))
-    report_template = env.get_template('report.html')
+def main(args):
+    """
+    The main.
+    :param args: script args
+    :return: status
+    """
+    # Compute reports
+    base_report = JUnitReport.report(os.path.join(args.tests_directory, '.'))
+    mutations_reports = [
+        JUnitReport.report(os.path.join(args.tests_directory, '.')),
+        JUnitReport.report(os.path.join(args.tests_directory, '.')),
+        JUnitReport.report(os.path.join(args.tests_directory, '.'))
+    ]
 
     # Render the report
-    report_rendered = report_template.render(
-        title='DevOps :: Mr.Statamutation :: Report',
-        prog_report=base_report,
-        mutations_reports=mutations_reports
-    )
-
-    # Write the report to the file
-    with open('statam_report.html', 'w') as file:
-        file.write(report_rendered)
+    ReportRenderer(base_report, mutations_reports).render_html(args.output)
 
 
 def get_parser():
+    """
+    Initialize command-line parser with default and optional arguments.
+    :return: parser
+    """
     # Enable command-line parsing
     parser = argparse.ArgumentParser()
 
-    # Needed arguments
-    parser.add_argument("mutants_directory",
-                        help="all mutants main directory")
+    # Default arguments
+    parser.add_argument('tests_directory',
+                        help='all tests main directory')
 
     # Optional arguments
-    parser.add_argument("--testsdir",
-                        help="tests results sub-directory",
-                        default="target/surefire-reports")
     parser.add_argument('-o', '--output',
-                        help="output HTML report file (will be overwritten)",
-                        default='statam_report.html')
+                        help='HTML report output file (will be overwritten)',
+                        default='./statam_report.html')
 
     return parser
 
 
 if __name__ == "__main__":
-    # Parse command-line args
-    args = get_parser().parse_args()
-
     # Start the main
-    sys.exit(main())
+    sys.exit(main(
+        # Parse command-line args
+        get_parser().parse_args()
+    ))
