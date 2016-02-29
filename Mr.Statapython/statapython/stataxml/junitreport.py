18b1b8083b5562d21d0694c3be79933fb23b461d
Kevin Buisson <buisson.kevin2@gmail.com>
2016-02-29 19:30:58 +0100

    [Update] Add some infos to reports

--- Mr.Statapython/statapython/stataxml/junitreport.py
+++ Mr.Statapython/statapython/stataxml/junitreport.py
@@ -9,7 +9,10 @@ COUNTS_TYPES = ['tests', 'failures', 'errors', 'skipped']
 class JUnitReport:
     """ JUnit report object model. """
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        self.compilation_failed = False
+
         self.testsuites = []
         self.counts = {'success': 0}
 
@@ -34,22 +37,26 @@ class JUnitReport:
         self.counts['success'] += int(testsuite['tests']) * 2  # x2 because of -= in loop
 
     @staticmethod
-    def report(directory):
+    def report(name, directory):
         """
         List all JUnit XML tests suites reports and compute a report.
         :param directory: JUnit XML reports directory
         :return: report of XML files
         """
-        report = JUnitReport()
+        report = JUnitReport(name)
 
-        # Get all files in the specified direction
-        for file in os.listdir(directory):
-            # Check if the file is a test XML report file
-            if file.startswith('TEST-') and file.endswith('.xml'):
-                # Add the test suite results
-                report.add_testsuite(
-                    # Parse the test suite
-                    JUnitXMLReader.read_testsuite(os.path.join(directory, file))
-                )
+        # Check if compilation was not a failure
+        if os.path.isfile(os.path.join(directory, '.compilation_failed')):
+            report.compilation_failed = True
+        else:
+            # Get all files in the specified direction
+            for file in os.listdir(directory):
+                # Check if the file is a test XML report file
+                if file.startswith('TEST-') and file.endswith('.xml'):
+                    # Add the test suite results
+                    report.add_testsuite(
+                        # Parse the test suite
+                        JUnitXMLReader.read_testsuite(os.path.join(directory, file))
+                    )
 
         return report
