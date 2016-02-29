5b1b6b3dc132f0a77ef6f1c9050f16500bab61bb
Kevin Buisson <buisson.kevin2@gmail.com>
2016-02-29 19:31:17 +0100

    [Update] Statapython main

---
+++ Mr.Statapython/statapython/main.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import sys
+
+from . import MutationsTester
+from .statareport import Reporting
+from .statautils import Directory, Logger
+
+""" Report filename """
+REPORT_FILENAME = 'statam_report.html'
+
+""" Neutral processor (original tests) """
+NEUTRAL_PROCESSOR = 'fr.polytech.devops.g1.stataspoon.processors.NeutralProcessor'
+
+
+def main(args):
+    """
+    Main function.
+    :param args: command-line arguments
+    """
+    if args.project:
+        # Go to project directory
+        os.chdir(os.path.abspath(args.project))
+
+    # First, clean directory
+    Directory.delete(args.report_directory)
+
+    # Create mutator tester, and execute original tests
+    mutator = MutationsTester(args.tests_directory, args.report_directory)
+    mutator.process(NEUTRAL_PROCESSOR, args.original)
+
+    # Execute mutations
+    mutator.process('fr.polytech.devops.g1.stataspoon.processors.operators.binary.PlusToMinusProcessor')
+
+    # Compute reporting
+    report_file = os.path.join(args.report_directory, REPORT_FILENAME)
+    Logger.log('=============== Generating report ===============', True)
+    Reporting(args.report_directory, args.original).report(report_file)
+    Logger.log('Report accessible at : %s' % report_file)
+
+
+def get_parser():
+    """
+    Initialize command-line parser with default and optional arguments.
+    :return: parser
+    """
+    # Enable command-line parsing
+    parser = argparse.ArgumentParser()
+
+    # Optional arguments
+    parser.add_argument('-p', '--project',
+                        help='project main directory')
+
+    # parser.add_argument('-m', '--mutations',
+    #                    help='mutations configuration file')
+
+    parser.add_argument('-r', '--report-directory',
+                        help='report output directory (generated report)',
+                        default='./target/statam-report')
+
+    parser.add_argument('-t', '--tests-directory',
+                        help='tests directory (output when tests are executed)',
+                        default='./target/surefire-reports')
+
+    parser.add_argument('-g', '--original',
+                        help='original (not mutated) tests directory',
+                        default='_Original_')
+    return parser
+
+
+# Main execution
+if __name__ == "__main__":
+    # Enable logging
+    Logger.ENABLED = True
+    Logger.log('=============== Welcome in StataReporting ===============', True)
+
+    # Start the main
+    sys.exit(main(
+        # Parse command-line args
+        get_parser().parse_args()
+    ))
---
+++ Mr.Statapython/statapython/mutationstester.py
@@ -0,0 +1,63 @@
+import logging
+import os
+
+from .statautils import Directory, Logger
+
+""" Logger """
+logger = logging.getLogger()
+
+
+class MutationsTester:
+    """ Mutations tests launcher. """
+
+    def __init__(self, tests_directory, report_directory):
+        self.tests_directory = tests_directory
+        self.report_directory = report_directory
+
+    def clean(self):
+        """
+        Delete tests directory.
+        """
+        Directory.delete(self.tests_directory)
+
+    def process(self, processor, output_dir=None):
+        """
+        Execute mutation and tests.
+        :param processor: processor to apply
+        :param output_dir: output directory
+        """
+        self.clean()
+        Logger.log('========================== Mutation with processor %s ==========================' % processor, True)
+
+        # Prepare command to execute
+        command = 'mvn package -Dstataprocessor="' + processor + '"'
+        Logger.log('Executing command: %s' % command, False, True)
+
+        # Post-process results
+        if not (
+                self.postprocess(
+                    # Execute mutations and tests
+                    os.system(command),
+                    # Final output directory
+                    os.path.join(self.report_directory, (output_dir if output_dir else processor))
+                )):
+            Logger.log('Compilation failed for processor: %s' % processor, True)
+
+    def postprocess(self, status, output_dir):
+        """
+        Post-process.
+        :param status: tests status
+        :param output_dir: output directory
+        :return True if compilation succeeded
+        """
+        try:
+            # Copy results to output report directory
+            Logger.log('Copying files from "%s" to "%s"...' % (self.tests_directory, output_dir), True)
+            Directory.copy(self.tests_directory, output_dir)
+            return True
+        # Compilation failed
+        except FileNotFoundError:
+            # Compilation failed
+            if status > 0:
+                # Create an empty file to indicate the compilation failed
+                Directory.touch(os.path.join(output_dir, '.compilation_failed'))
