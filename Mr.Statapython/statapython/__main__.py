d83eae79cce2d56ccadff16c0fed99aefeeda2db
Kevin Buisson <buisson.kevin2@gmail.com>
2016-03-01 16:42:35 +0100

    [Update] Main process execution

--- Mr.Statapython/statapython/__main__.py
+++ Mr.Statapython/statapython/__main__.py
@@ -5,8 +5,8 @@ import argparse
 import os
 import sys
 
-from . import MutationsTester
 from .statareport import Reporting
+from .stataspoon import MutationsTester
 from .statautils import Directory, Logger
 
 """ Report filename """
@@ -25,24 +25,44 @@ def main(args):
         # Go to project directory
         os.chdir(os.path.abspath(args.project))
 
-    # First, clean directory
-    try:
-        Directory.delete(args.report_directory)
-    except:
-        Logger.log('[Warning] Error on cleaning repôrt directory ("%s")' % args.report_directory)
+    # Clean the report directory
+    Logger.log('Pre-cleaning report directory "%s"' % args.report_directory, True)
+    pre_clean(args.report_directory)
 
     # Create mutator tester, and execute original tests
-    mutator = MutationsTester(args.tests_directory, args.report_directory)
-    mutator.process(NEUTRAL_PROCESSOR, args.original)
+    mutator = MutationsTester(args.tests_directory, args.report_directory, not args.keep_temp)
+    mutator.process(args.original)
 
     # Execute mutations
-    mutator.process('fr.polytech.devops.g1.stataspoon.processors.operators.binary.PlusToMinusProcessor')
+    mutator.process('BadassMutations',
+                    ('fr.polytech.devops.g1.stataspoon.processors.operators.binary.PlusToMinusProcessor',
+                     'fr.polytech.devops.g1.stataspoon.processors.operators.binary.InfToSupProcessor',
+                     'fr.polytech.devops.g1.stataspoon.processors.operators.binary.InfEqToSupEqProcessor'))
 
     # Compute reporting
     report_file = os.path.join(args.report_directory, REPORT_FILENAME)
     Logger.log('=============== Generating report ===============', True)
     Reporting(args.report_directory, args.original).report(report_file)
-    Logger.log('Report accessible at : %s' % os.path.abspath(report_file))
+    Logger.log('Report accessible at: %s' % os.path.abspath(report_file))
+
+
+def pre_clean(directory):
+    """
+    Pre-clean the project.
+    :param directory: report directory
+    :return:
+    """
+    # Clean directory
+    try:
+        Directory.delete(directory)
+    except:
+        Logger.log('[Warning] Error on cleaning report directory ("%s")' % directory)
+
+    # Create a new one
+    try:
+        Directory.create(directory)
+    except:
+        Logger.log('[Warning] Error on creating report directory ("%s")' % directory)
 
 
 def get_parser():
@@ -70,7 +90,11 @@ def get_parser():
 
     parser.add_argument('-g', '--original',
                         help='original (not mutated) tests directory',
-                        default='_Original_')
+                        default='_original_')
+
+    parser.add_argument('-k', '--keep-temp',
+                        help='enable/disable temporary file cleaning',
+                        default=False)
     return parser
 
 
@@ -78,7 +102,7 @@ def get_parser():
 if __name__ == "__main__":
     # Enable logging
     Logger.ENABLED = True
-    Logger.log('=============== Welcome in StataReporting ===============', True)
+    Logger.log('=============== <3 - Welcome in Mr.Statamutation project - <3 ===============', True)
 
     # Start the main
     sys.exit(main(
