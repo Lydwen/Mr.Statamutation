3626a1e38c41e708694914143d81e0fcaf736133
Tom DALL'AGNOL <dallagnol.tom@etu.unice.fr>
2016-03-10 17:20:59 +0100

    Add the selector into process()

--- Mr.Stataspoon/src/main/java/fr/polytech/devops/g1/stataspoon/processors/operators/binary/ProcessorModel.java
+++ Mr.Stataspoon/src/main/java/fr/polytech/devops/g1/stataspoon/processors/operators/binary/ProcessorModel.java
@@ -1,8 +1,11 @@
 package fr.polytech.devops.g1.stataspoon.processors.operators.binary;
 
+import fr.polytech.devops.g1.stataspoon.processors.selectors.ISelector;
+import fr.polytech.devops.g1.stataspoon.processors.selectors.SelectorController;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.declaration.CtClass;
 
 /**
  * Processor Model.
@@ -13,18 +16,25 @@ public abstract class ProcessorModel extends AbstractProcessor<CtBinaryOperator>
 
     protected BinaryOperatorKind replaceThis;
     protected BinaryOperatorKind byThis;
+    private ISelector selector;
+
 
     protected ProcessorModel(BinaryOperatorKind replaceThis, BinaryOperatorKind byThis){
         this.replaceThis = replaceThis;
         this.byThis = byThis;
+        this.selector = SelectorController.getInstance().getSelector();
+
     }
 
     @Override
-    public void process(CtBinaryOperator operator) {
-        // Find
-        if (!operator.getKind().equals(replaceThis)) return;
+    public void process(CtBinaryOperator op) {
+        CtClass parent = op.getParent(CtClass.class);
+        if(selector.isSelected(parent.getQualifiedName())){
+            // Find
+            if (!op.getKind().equals(replaceThis)) return;
 
-        // Replace
-        operator.setKind(byThis);
+            // Replace
+            op.setKind(byThis);
+        }
     }
 }
